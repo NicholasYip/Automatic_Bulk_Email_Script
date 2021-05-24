84b113b31344ffe40e6af94f8179f99bd1a27f02
NicholasYip <yiphoiye.n@gmail.com>
2021-05-23 21:56:41 -0400

    attachments

--- Frosh_Email_Sender.py
+++ Frosh_Email_Sender.py
@@ -9,7 +9,7 @@ from string import Template
 
 # automatic email script, originally made for McGill PTOT Orientation Week 2021 by Nicholas Yiphoiyen
 # three .txt files are needed, see examples of .txt files in the repo
-# 1. NAME_EMAIL.txt --> has the names and emails of the sponsors separated by a comma, one sponsor per line (I.E firstname lastname ,email)
+# 1. NAME_EMAIL.txt --> has the names and emails of the sponsors separated by a comma, one sponsor per line (I.E firstname lastname, email)
 # 2. ALREADY_SENT_EMAILS.txt --> emails, name of the sponsors that were properly sent an email
 # 3. TEMPLATE_TEXT --> text that you want to send to the sponsors, set the name of the person as ${PERSON_NAME}
 # you need to set the subject of the email and add any attachments you want to send with the email as an array (don't forget to put the file extension ex: .pdf, .png)
