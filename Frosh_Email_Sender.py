d3fa0dd5fb7136c18c08acc5872627651452ef7e
Nicholas Yiphoiyen <61575740+NicholasYip@users.noreply.github.com>
2021-06-13 16:18:34 -0400

    Update Frosh_Email_Sender.py

--- Frosh_Email_Sender.py
+++ Frosh_Email_Sender.py
@@ -12,7 +12,9 @@ from string import Template
 # 1. NAME_EMAIL.txt --> has the names and emails of the sponsors separated by a comma, one sponsor per line (I.E firstname lastname, email)
 # 2. ALREADY_SENT_EMAILS.txt --> emails, name of the sponsors that were properly sent an email
 # 3. TEMPLATE_TEXT --> text that you want to send to the sponsors, set the name of the person as ${PERSON_NAME}
+# put these three txt files above in the folder called 'emails'
 # you need to set the subject of the email and add any attachments you want to send with the email as an array (don't forget to put the file extension ex: .pdf, .png)
+# put these attachments in a folder called 'attachments'
 # don't forget to put your email address, password also
 # Two-step verification must be disabled on your email account & allow less-secure apps to access your account (just google it)
 # CAREFUL : Test it out with your personal email before sending the emails to sponsors
